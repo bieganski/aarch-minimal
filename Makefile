e74fcc01644dc60d83f719a7763996212c561cd2
Mateusz Bieganski <bieganski.m@wp.pl>
2025-04-01 09:37:14 +0200

    RISC-V: segfault in GNU linker

--- Makefile
+++ Makefile
@@ -2,10 +2,16 @@
 SRCS := header.S main.c pl011.c
 OBJS := header.o main.o pl011.o
 
-TOOLCHAIN := aarch64-linux-gnu
+# TOOLCHAIN := aarch64-linux-gnu
+# ARCH_FLAGS := -mcpu=cortex-a57+nofp
+
+TOOLCHAIN := riscv64-linux-gnu
+ARCH_FLAGS := 
+
+
 
 all:
-	$(TOOLCHAIN)-gcc -mcpu=cortex-a57+nofp -O3 -fno-stack-protector  $(SRCS) -fPIE -fPIC -static-pie -static -nostdlib -nostartfiles -nodefaultlibs -T kernel.lds -o kernel.elf -Wl,--build-id=none
+	$(TOOLCHAIN)-gcc $(ARCH_FLAGS) -O3 -fno-stack-protector  $(SRCS) -fPIE -fPIC -static-pie -static -nostdlib -nostartfiles -nodefaultlibs -T kernel.lds -o kernel.elf -Wl,--build-id=none
 	llvm-objcopy --input-target elf64-littleaarch64 --output-target binary kernel.elf kernel.bin
 
 run:
--- main.c
+++ main.c
@@ -1,24 +1,32 @@
-#include "pl011.h"
+#include <stdint.h>
+// #include "pl011.h"
+#include "uart.h"
 
 #define UART_BASE 0x9000000 // matches QEMU '-machine virt'. determined using 'qemu-system-XXX -machine virt -machine dumpdtb=qemu.dtb'
 #define CLOCK_FREQ 24000000  // must match device tree
 
-void print_hex(struct pl011* dev, char* prefix, uint32_t value) {
-    while (*prefix) {
-        pl011_putc(dev, *prefix++);
+#define pl011_putc(dev, char) uart_putc(char)
+
+void uart_send(char* data) {
+    while (*data) {
+        uart_putc(*data++);
     }
-    pl011_putc(dev, ' ');
+}
+
+void print_hex(char* prefix, uint32_t value) {
+    uart_send(prefix);
+    uart_putc(' ');
     for (int shift = 28; shift >= 0; shift -= 4) {
         uint8_t nibble = (value >> shift) & 0xF;
-        pl011_putc(dev, nibble < 10 ? '0' + nibble : 'A' + nibble - 10);
+        uart_putc(nibble < 10 ? '0' + nibble : 'A' + nibble - 10);
     }
-    pl011_send(dev, "\r\n", 2);
+    uart_send("\r\n");
 }
 
 int main (int argc, char**argv){
-    struct pl011 dev;
-    pl011_setup(&dev, UART_BASE, CLOCK_FREQ);
 
-    print_hex(&dev, "\nHello from your 'but-we-have-kernel-at-home' kernel!", (0x4200 | 0x42));
+    uart_init();
+
+    print_hex("\nHello from your 'but-we-have-kernel-at-home' kernel!", (0x4200 | 0x42));
     return 0;
 }
